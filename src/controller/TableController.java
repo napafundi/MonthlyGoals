70789a3b570293ec2910e044a5794d677dd989da
Nick Pafundi <napafundi@gmail.com>
2019-09-02 13:45:40 -0400

    working on TableController's fillTable() method

--- src/controller/TableController.java
+++ src/controller/TableController.java
@@ -1,5 +1,26 @@
 package controller;
 
+import java.sql.*;
+import java.util.ArrayList;
+
+import javax.swing.JTable;
+
 public class TableController {
 
+	public void fillTable(Connection conn, JTable table) {
+		ResultSet rs;
+		try {
+			ResultSetMetaData md = rs.getMetaData();
+			int columnCount = md.getColumnCount();
+			ArrayList<String> columns = new ArrayList<String>(columnCount);
+			
+			// (store column names)
+			for (int i =1; i <= columnCount; i++) {
+				columns.add(md.getColumnName(i));
+			}
+			
+			ArrayList<String>[][] data = new ArrayList[0][columnCount];
+		}
+		
+	}
 }
